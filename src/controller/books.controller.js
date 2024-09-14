2c148568623efecfd0a2b3e90c941e475ecc4392
Gaganrobin8831 <gaganrobin71@gmail.com>
2024-09-14 13:05:01 +0530

    changes

--- src/controller/books.controller.js
+++ src/controller/books.controller.js
@@ -1,23 +1,3 @@
-// const Author = require('../models/user.model');
-// const Book = require('../models/books.model');
-
-// async function HanleGet(req,res) {
-//     try {
-   
-               
-//         const users = await Book.find().populate('user');
-        
-//         res.render('showBooks', { users });
-//     } catch (err) {
-//         console.error(err);
-//         res.status(500).render('error', {
-//             message: 'An error occurred while retrieving users',
-//             details: err.message
-//         });
-//     }
-// }
-
-// module.exports = {HanleGet}
 
 const Book = require('../models/books.model');
 const { ApiError } = require('../utilits/ApiError.utilitis'); // Adjust the path as necessary
