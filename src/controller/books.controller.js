30ce7309513bef0118df15914706754fda160d99
Gaganrobin8831 <gaganrobin71@gmail.com>
2024-09-14 11:11:42 +0530

    changes

--- src/controller/books.controller.js
+++ src/controller/books.controller.js
@@ -3,13 +3,10 @@ const Book = require('../models/books.model');
 
 async function HanleGet(req,res) {
     try {
-        // Retrieve all users
-        // const users = await book.find();
+   
                
         const users = await Book.find().populate('user');
-         // This will populate the `books` array with book documents
-
-        // Render the 'users' view with the list of users
+        
         res.render('showBooks', { users });
     } catch (err) {
         console.error(err);
